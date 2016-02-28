6278a4962073f8ad95a21816e90f190e816b8c99
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-27 23:21:15 -0500

    adjust argument names and descriptions

--- index.js
+++ index.js
@@ -17,15 +17,15 @@ program
 
 program
     .command(`macro`)
-    .option(`-t, --title <title>`, `Title of macro`)
+    .option(`-t, --title <title>`, `Set title of macro`)
     .option(`-f, --file <path>`, `Path to comment text file`)
     .option(`-w, --write`, `Open editor to write comment content`)
     .option(`-T, --tags <tags>`, `Append tags (space delineated)`)
     .option(`-i, --inactive`, `Make macro inactive`)
     .option(`-p, --private`, `Set comment mode to private`)
     .option(`-l, --list`, `List all macros`)
-    .option(`-u, --update <id>`, `Update macro`, parseInt)
-    .option(`-o, --open [id]`, `Open the macro in ZenDesk`, parseInt)
+    .option(`-u, --update <macro_id>`, `Update macro`, parseInt)
+    .option(`-o, --open [macro_id]`, `Open the macro in ZenDesk`, parseInt)
     .description(`Add a macro to ZenDesk`)
     .action ((options) => {
         lib.getScript(`macro`)(options);
