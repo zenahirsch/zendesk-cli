c0e08fa609339283ea89e815e9c0b5951d6eec82
Zena Hirsch <hirsch.zena@gmail.com>
2016-02-27 22:30:13 -0500

    implement some more stuff

--- index.js
+++ index.js
@@ -9,15 +9,23 @@ program
     .description(pkg.description);
 
 program
+    .command(`init <username> <token> <remoteUri>`)
+    .description(`Initialize this utility`)
+    .action((username, token, remoteUri) => {
+        lib.getScript(`init`)(username, token, remoteUri);
+    });
+
+program
     .command(`macro`)
+    .option(`-t, --title <title>`, `Title of macro`)
     .option(`-f, --file <path>`, `Path to comment text file`)
     .option(`-w, --write`, `Open editor to write comment content`)
-    .option(`-t, --tags <tags>`, `Append tags (space delineated)`)
-    .option(`-A, --inactive`, `Make macro inactive`)
+    .option(`-T, --tags <tags>`, `Append tags (space delineated)`)
+    .option(`-i, --inactive`, `Make macro inactive`)
     .option(`-p, --private`, `Set comment mode to private`)
     .option(`-l, --list`, `List all macros`)
     .option(`-u, --update <id>`, `Update macro`, parseInt)
-    .option(`-o, --open [id]`, `Open the macro in ZenDesk`)
+    .option(`-o, --open [id]`, `Open the macro in ZenDesk`, parseInt)
     .description(`Add a macro to ZenDesk`)
     .action ((options) => {
         lib.getScript(`macro`)(options);
